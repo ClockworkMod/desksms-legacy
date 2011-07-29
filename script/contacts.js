e39eb202fd10339e70531e0b2a6b9de17a819619
Koushik Dutta <koushd@gmail.com>
2011-07-29 15:30:23 -0700

    contact merge bug;

--- script/contacts.js
+++ script/contacts.js
@@ -35,9 +35,11 @@ var contacts = new function() {
     if (list == null)
       list = this.list;
     var numbersOnly = this.numbersOnly(number);
+    if (numbersOnly == null || numbersOnly.length < 4)
+      return null;
     var ret = null;
     $.each(list, function(index, entry) {
-      if (entry.numbersOnly == null)
+      if (entry.numbersOnly == null || entry.numbersOnly.length < 4)
         return;
       if (entry.numbersOnly.indexOf(numbersOnly) != -1 || numbersOnly.indexOf(entry.numbersOnly) != -1)
         ret = entry;
