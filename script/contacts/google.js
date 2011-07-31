6013504c880713964cbf96288b077692dd498040
Koushik Dutta <koushd@gmail.com>
2011-07-30 23:48:20 -0700

    Revert "test caching"
    This reverts commit 3ea53785feb8ddae65f6b13abecb2baef2135df0.

--- script/contacts/google.js
+++ script/contacts/google.js
@@ -15,7 +15,6 @@ var googleContacts = new function() {
         return;
       
       jsonp('https://www.google.com/m8/feeds/contacts/default/full?max-results=10000&alt=json', function(err, data) {
-        return;
         if (err) {
           console.log(err);
           return;
