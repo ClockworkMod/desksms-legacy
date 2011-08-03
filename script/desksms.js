d0c02ea91a9b143e9a34cb33d84f28743bd2d8f1
Koushik Dutta <koushd@gmail.com>
2011-08-03 06:45:26 -0700

    new push

--- script/desksms.js
+++ script/desksms.js
@@ -95,7 +95,7 @@ var desksms = new function() {
   }
   
   this.push = function(cb) {
-    $.get('http://desksms.deployfu.com:9980/wait/' + encodeURIComponent(desksms.registrationId) + "?nonce=" + Date.now(), function(data) {
+    $.get('http://desksmspush.deployfu.com:9980/wait/' + encodeURIComponent(desksms.registrationId) + "?nonce=" + Date.now(), function(data) {
       desksms.push(cb);
       cb(null, data);
     })
